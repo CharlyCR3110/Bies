52fedd8f0cca29192a8ee95d58207610413b25ff
Charly <carlosgarita3110@gmail.com>
2024-09-02 15:32:57 -0600

    refactor: Update EstadoCarronia to use Alimento instead of Carronia

--- src/main/java/bies/ente/insecto/estado/carronia/EstadoCarronia.java
+++ src/main/java/bies/ente/insecto/estado/carronia/EstadoCarronia.java
@@ -1,6 +1,6 @@
 package bies.ente.insecto.estado.carronia;
 
-import bies.alimentacion.carronia.Carronia;
+import bies.alimentacion.Alimento;
 import bies.ente.insecto.estado.IEstadoInsecto;
 
 /**
@@ -20,24 +20,24 @@ import bies.ente.insecto.estado.IEstadoInsecto;
  */
 public class EstadoCarronia implements IEstadoInsecto {
 
-    /** Referencia al objeto `Carronia` que representa el estado de carroña del insecto. */
-    private Carronia carronia;
+    /** Referencia al record `Alimento` que representa el estado de carroña del insecto. */
+    private Alimento carronia;
 
     /**
      * Constructor que inicializa el estado de carroña con un objeto `Carronia`.
      * 
      * @param carronia El objeto `Carronia` que representa el estado de carroña.
      */
-    public EstadoCarronia(Carronia carronia) {
+    public EstadoCarronia(Alimento carronia) {
         this.carronia = carronia;
     }
 
     /**
-     * Obtiene el objeto `Carronia` asociado a este estado de carroña.
+     * Obtiene el objeto `Alimento` asociado a este estado de carroña.
      * 
      * @return El objeto `Carronia` asociado a este estado.
      */
-    public Carronia getCarronia() {
+    public Alimento getCarronia() {
         return carronia;
     }
 
